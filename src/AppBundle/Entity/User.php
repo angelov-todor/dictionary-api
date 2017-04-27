763263c5983a677d507218a701e9ae15102d31b8
Todor Angelov <todor.angelov@programista.pro>
2017-04-27 13:35:53 +0300

    try docs

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -18,6 +18,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *          "authenticate"={
  *              "route_name"="authenticate",
  *               "swagger_context" = {
+ *                  "tags" = ["Authentication"],
  *                  "parameters" = {
  *                      {
  *                          "name" = "user",
