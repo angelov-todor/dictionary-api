c0ca29b9682aa237040ab4c959ecb5c10f77586a
Todor's Laptop <todor_an@yahoo.com>
2017-05-05 20:01:05 +0300

    metadata entity and resource

--- src/AppBundle/Entity/Metadata.php
+++ src/AppBundle/Entity/Metadata.php
@@ -1,15 +1,94 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: todor
- * Date: 4.5.2017 г.
- * Time: 22:06 ч.
- */
+declare(strict_types=1);
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+
+/**
+ * Metadata
+ *
+ * @ORM\Table(name="metadata")
+ * @ORM\Entity
+ */
 class Metadata
 {
+    const TYPE_NUMBER = 'number';
+    const TYPE_TEXT = 'text';
 
+    protected static $types = [
+        self::TYPE_NUMBER,
+        self::TYPE_TEXT
+    ];
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50, nullable=false)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", columnDefinition="ENUM('number', 'text')", nullable=false)
+     */
+    protected $type = self::TYPE_TEXT;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Metadata
+     */
+    public function setName(string $name): Metadata
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return Metadata
+     */
+    public function setType(string $type): Metadata
+    {
+        if (!in_array($type, self::$types)) {
+            throw new \InvalidArgumentException('Invalid type given for metadata');
+        }
+        $this->type = $type;
+        return $this;
+    }
 }
