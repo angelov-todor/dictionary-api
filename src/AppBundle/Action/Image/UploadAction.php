1cc7963a0a30fd92f3192b7edc408da93403b68c
Todor's Laptop <todor_an@yahoo.com>
2017-05-18 19:33:29 +0300

    upload action

--- src/AppBundle/Action/Image/UploadAction.php
+++ src/AppBundle/Action/Image/UploadAction.php
@@ -35,6 +35,34 @@ class UploadAction
      */
     public function imageUploadAction(Request $request)
     {
-        return new JsonResponse();
+        $filename = $request->get('filename');
+        $data = $request->get('data');
+
+
+        return new JsonResponse(['path' => getcwd()]);
+    }
+
+    /**
+     * @param string $base64
+     * @param string $filename
+     * @return string
+     */
+    protected function base64ToJpeg(string $base64, string $filename): string
+    {
+        // open the output file for writing
+        $ifp = fopen($filename, 'wb');
+
+        // split the string on commas
+        // $data[ 0 ] == "data:image/png;base64"
+        // $data[ 1 ] == <actual base64 string>
+        $data = explode(',', $base64);
+
+        // we could add validation here with ensuring count( $data ) > 1
+        fwrite($ifp, base64_decode($data[1]));
+
+        // clean up the file resource
+        fclose($ifp);
+
+        return $filename;
     }
 }
