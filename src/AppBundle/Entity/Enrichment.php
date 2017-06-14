14e1827eb745ac6d6fdff639c1c50ae900bd4608
Todor's Laptop <todor_an@yahoo.com>
2017-06-14 18:07:12 +0300

    (feat) image metadata auto generation

--- src/AppBundle/Entity/Enrichment.php
+++ src/AppBundle/Entity/Enrichment.php
@@ -10,7 +10,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  * Class Enrichment
  * @package AppBundle\Entity
  * @ApiResource(
- *     collectionOperations={"get"={"method"="GET"}}, itemOperations={},
+ *     collectionOperations={}, itemOperations={"get"={"method"="GET"}},
  *     attributes={"normalization_context"={"groups"={"get_enrichment"}}}
  * )
  */
